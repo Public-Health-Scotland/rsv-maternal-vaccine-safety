5aa23008eab83ccde03054b84c5b8c0eca8c6824
Lucy Cullen <52078605+lucyde02@users.noreply.github.com>
2026-05-11 17:06:54 +0100

    Initial commit

---
+++ code/code.R
@@ -0,0 +1,36 @@
+##########################################################
+# Name of script
+# Publication (delete if not applicable)
+# Original author(s): Original author(s)
+# Original date: 2026-02-23
+# Latest update author (delete if using version control)
+# Latest update date (delete if using version control)
+# Latest update description (delete if using version control)
+# Type of script (e.g. extraction, preparation, modelling)
+# Written/run on RStudio server 2025.9.1.401.2 and R 4.5.1
+# Description of content
+# Approximate run time
+#
+# PWB recommended options #
+# CPUs:
+# Memory:
+##########################################################
+
+### 1 - Housekeeping ----
+# This section should be the only section of the script which requires manual changes
+# for future updates and includes:
+#   loading packages
+source("code/packages.R") # Remove this line if loading packages here
+#   setting file paths and extract dates
+#   defining functions
+source("code/functions.R") # Remove this line if defining functions here
+#   setting plot parameter
+#   specifying codes (e.g. ICD-10 codes)
+
+### 2 Section Heading ----
+
+### 3 Section Heading ----
+
+# quit(save = "no") # Uncomment this to automatically close the R session when finished
+
+### END OF SCRIPT ###
